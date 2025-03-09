03dd1d4b1d8c7650848386b27c265b7d0ecc24b2
prashantrahul141 <prashantrahul141@protonmail.com>
2025-03-09 12:04:05 +0530

    init

---
+++ content/_index.md
@@ -0,0 +1,6 @@
++++
+title= "Prashant Rahul"
+template = "homepage.html"
++++
+
+I am a software developer working in a lot of fields with a specialization in building and tinkering compilers and interpreters. I also work on creating full-stack web experiences, emulators, graphics APIs, games and cross platform applications.
---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "dev environment";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages."x86_64-linux";
+    in
+    {
+      devShells."x86_64-linux".default = pkgs.mkShell {
+        packages = with pkgs; [
+          zola
+        ];
+      };
+    };
+}
