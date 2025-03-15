96e96ab5e64636d48947de1740b0618780911cbd
prashantrahul141 <prashantrahul141@protonmail.com>
2025-03-15 23:26:24 +0530

    remove uncessary properties

--- build-gh-repos.sh
+++ build-gh-repos.sh
@@ -7,7 +7,7 @@ rm -f $output_file
 
 curl -s -X POST -H "Authorization: Bearer $GRAPHQL_GITHUB_TOKEN" \
      -H "Content-Type: application/json" \
-     -d '{"query": "{ user(login: \"prashantrahul141\") { pinnedItems(first: 6, types: [REPOSITORY]) { totalCount edges { node { ... on Repository { name repositoryTopics(first: 3) { edges { node { id topic { id name } } } } primaryLanguage { id name color } description url stargazerCount owner { login } } } } } } }"}' \
+     -d '{"query": "{ user(login: \"prashantrahul141\") { pinnedItems(first: 6, types: [REPOSITORY]) { edges { node { ... on Repository { name primaryLanguage { name } description url stargazerCount } } } } } }"}' \
      https://api.github.com/graphql | yq -t > $output_file
 
 echo "Done fetching..."
