53390f00582e686502ab5a994813eb8f864fbebf
prashantrahul141 <prashantrahul141@protonmail.com>
2025-08-20 19:19:21 +0530

    feat: add work experience, fix styling
    Signed-off-by: prashantrahul141 <prashantrahul141@protonmail.com>

--- build-resume.sh
+++ build-resume.sh
@@ -3,7 +3,7 @@
 PRE_PROJ_FILE="./static/resume/pre-proj.tex"
 POST_PROJ_FILE="./static/resume/post-proj.tex"
 OUTPUT_FILE="./static/resume/output.tex"
-CV_FILE="./static/Prashant-Rahul-CV.pdf"
+CV_NAME="Prashant-Rahul-CV"
 
 QUERY='{"query": "{ user(login: \"prashantrahul141\") { pinnedItems(first: 6, types: [REPOSITORY]) { edges { node { ... on Repository { name primaryLanguage { name } description url stargazerCount repositoryTopics(first: 3) { edges { node { topic { name } } } } } } } } } }"}'
 
@@ -19,11 +19,13 @@ echo "$RESPONSE" | jq -r '
   "\\noindent\\textbf{" + .name + "} \\textit{\\footnotesize (" +
   ( [(.repositoryTopics.edges[].node.topic.name)] | join(", ") ) +
   ") } \\hfill \\texttt{\\href{" + .url + "}{[link]}} \\\\ \n" +
-  (.description // "No description available") + " \\\\ \n"
+  (.description // "No description available") + " \\vspace{0.5em} \n"
 ' | sed 's/C#/C\\#/g' >> "$OUTPUT_FILE"
+
+echo "\\vspace{-0.5em}" >> "$OUTPUT_FILE"
 
 test -f "$POST_PROJ_FILE" && cat "$POST_PROJ_FILE" >> "$OUTPUT_FILE"
 
-pdflatex -output-directory=./static/ -jobname="Prashant-Rahul-CV" "$OUTPUT_FILE"
+pdflatex -output-directory=./static/ -jobname="$CV_NAME" "$OUTPUT_FILE"
 
 printf "\n\nFinished building resume....\n"
